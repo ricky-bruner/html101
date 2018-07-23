45cdb716f2bb047ae7867dbe5c4fd2c344d9a7f0
ricky-bruner <alteredecho07@yahoo.com>
2018-07-23 09:38:53 -0500

    added data to datastructure

---
+++ js/magicdata.js
@@ -0,0 +1,203 @@
+const magicWorld = {
+    students:[
+        {
+            gryffindor: [
+                {
+                    name: "Harry Potter",
+                    imageGif: "../images/harrygif.gif",
+                    imageIllustration: "../images/harrypotter.jpg",
+                    bio: "",
+                    house: "Gryffindor"
+                },
+                {
+                    name: "Ronald Weasley",
+                    image: "",
+                    bio: "",
+                    house: "Gryffindor",
+                },
+                {
+                    name: "Hermione Granger",
+                    image: "",
+                    bio: "",
+                    house: "Gryffindor",
+                },
+                {
+                    name: "Ginny Weasley",
+                    image: "",
+                    bio: "",
+                    house: "Gryffindor",
+                },
+                {
+                    name: "Neville Longbottom",
+                    image: "",
+                    bio: "",
+                    house: "Gryffindor",
+                }
+            ],
+            ravenclaw: [
+                {
+                    name: "Luna Lovegood",
+                    image: "",
+                    bio: "",
+                    house: "Ravenclaw"
+                },
+                {
+                    name: "Cho Chang",
+                    image: "",
+                    bio: "",
+                    house: "Ravenclaw"
+                },
+                {
+                    name: "Padma Patil",
+                    image: "",
+                    bio: "",
+                    house: "Ravenclaw"
+                },
+                {
+                    name: "Terry Boot",
+                    image: "",
+                    bio: "",
+                    house: "Ravenclaw"
+                }
+            ],
+            hufflepuff: [
+                {
+                    name: "Cedric Diggory",
+                    image: "",
+                    bio: "",
+                    house: "Hufflepuff"
+                },
+                {
+                    name: "Hannah Abbot",
+                    image: "",
+                    bio: "",
+                    house: "Hufflepuff"
+                },
+                {
+                    name: "Ernie McMillan",
+                    image: "",
+                    bio: "",
+                    house: "Hufflepuff"
+                },
+                {
+                    name: "Justin Finch-Fletchly",
+                    image: "",
+                    bio: "",
+                    house: "Hufflepuff"
+                }
+            ],
+            slytherin: [
+                {
+                    name: "Draco Malfoy",
+                    image: "",
+                    bio: "",
+                    house: "Slytherin"
+                },
+                {
+                    name: "Gregory Goyle",
+                    image: "",
+                    bio: "",
+                    house: "Slytherin"
+                },
+                {
+                    name: "Vincent Crabbe",
+                    image: "",
+                    bio: "",
+                    house: "Slytherin"
+                },
+                {
+                    name: "Blaise Zabini",
+                    image: "",
+                    bio: "",
+                    house: "Slytherin"
+                },
+            ]
+        }
+    ],
+    professors:[
+        {
+            consistant:[
+                {
+                    name: "Minerva McGonnagal",
+                    image: "",
+                    bio: "",
+                    house: "Gryffindor",
+                    course: "Transfiguration"
+                },
+                {
+                    name: "Severus Snape",
+                    image: "",
+                    bio: "",
+                    house: "Slytherin",
+                    course: "Potions"
+                },
+                {
+                    name: "Rubeus Hagrid",
+                    image: "",
+                    bio: "",
+                    house: "Gryffindor",
+                    course: "Care for Magical Creatures"
+                },
+                {
+                    name: "Filius Flitwick",
+                    image: "",
+                    bio: "",
+                    house: "Ravenclaw",
+                    course: "Charms"
+                },
+                {
+                    name: "Pamona Sprout",
+                    image: "",
+                    bio: "",
+                    house: "Hufflepuff",
+                    course: "Herbology"
+                },
+                {
+                    name: "Horace Slughorn",
+                    image: "",
+                    bio: "",
+                    house: "Slytherin",
+                    course: "Potions"
+                }
+            ],
+            darkArts: [
+                {
+                    name: "Quirinus Quirrell",
+                    image: "",
+                    bio: "",
+                    year: "Year 1"
+                },
+                {
+                    name: "Guilderoy Lockhart",
+                    image: "",
+                    bio: "",
+                    year: "Year 2"
+                },
+                {
+                    name: "Remus Lupin",
+                    image: "",
+                    bio: "",
+                    year: "Year 3"
+                },
+                {
+                    name: "Alastor Moody",
+                    image: "",
+                    bio: "",
+                    year: "Year 4"
+                },
+                {
+                    name: "Dolores Umbridge",
+                    image: "",
+                    bio: "",
+                    year: "Year 5"
+                },
+                {
+                    name: "Severus Snape",
+                    image: "",
+                    bio: "",
+                    year: "Year 6"
+                }
+            ]
+        }
+    ]
+};
